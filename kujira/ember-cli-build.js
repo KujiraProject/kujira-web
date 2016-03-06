9fffacb9a35ea55171e2bd412803d6a0bafc4566
Krzysztof Wasielewski <kwasielew@gmail.com>
2016-03-06 11:40:32 +0100

    Base layout(with routes) implemented with custom info-box component. Installed Mirage.

--- kujira/ember-cli-build.js
+++ kujira/ember-cli-build.js
@@ -4,8 +4,19 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
+    outputPaths: {
+      app: {
+        css: {
+          'app': '/assets/kujira.css',
+          'info-box': '/assets/info-box.css',
+          'navbar': '/assets/navbar.css',
+          'sidemenu': '/assets/sidemenu.css',
+        }
+      }
+  }
     // Add options here
   });
+
 
   // Use `app.import` to add additional libraries to the generated
   // output files.
@@ -19,6 +30,7 @@ module.exports = function(defaults) {
   // modules that you would like to import into your application
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
+  app.import('vendor/fonts/OpenSans-Regular.ttf', {destDir: 'fonts'});
 
   return app.toTree();
 };
