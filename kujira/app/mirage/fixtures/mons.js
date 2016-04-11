df9c3aa1cc3d8ab5440e4ce0ecdbc72fc2b5eaa4
Ukis <polatowski.lukasz@gmail.com>
2016-04-11 15:54:16 +0100

    displays table in api

---
+++ kujira/app/mirage/fixtures/mons.js
@@ -0,0 +1,26 @@
+export default [
+  {
+    "id": 0,
+    "addr": "12345",
+    "in-quorum": true,
+    "name": "figment000",
+    "rank": 0,
+    "server": "figment000.cluster0.com"
+  },
+  {
+    "id": 1,
+    "addr": "23456",
+    "in-quorum": true,
+    "name": "figment001",
+    "rank": 1,
+    "server": "figment001.cluster0.com"
+  },
+  {
+    "id": 2,
+    "addr": "34567",
+    "in-quorum": true,
+    "name": "figment002",
+    "rank": 2,
+    "server": "figment002.cluster0.com"
+  }
+];
---
+++ kujira/app/mirage/fixtures/pools.js
@@ -0,0 +1,58 @@
+export default [
+  {
+    "full": false,
+    "name": "data",
+    "quota-max-objects": 0,
+    "hashpspool": false,
+    "min-size": 1,
+    "crash-replay-interval": 0,
+    "pg-num": 64,
+    "pgp-num": 64,
+    "quota-max-bytes": 0,
+    "size": 2,
+    "id": 0,
+    "crush-ruleset": 2
+  },
+  {
+    "full": false,
+    "name": "metadata",
+    "quota-max-objects": 0,
+    "hashpspool": false,
+    "min-size": 1,
+    "crash-replay-interval": 0,
+    "pg-num": 64,
+    "pgp-num": 64,
+    "quota-max-bytes": 0,
+    "size": 2,
+    "id": 1,
+    "crush-ruleset": 2
+  },
+  {
+    "full": false,
+    "name": "rbd",
+    "quota-max-objects": 0,
+    "hashpspool": false,
+    "min-size": 1,
+    "crash-replay-interval": 0,
+    "pg-num": 64,
+    "pgp-num": 64,
+    "quota-max-bytes": 0,
+    "size": 2,
+    "id": 2,
+    "crush-ruleset": 2
+  },
+  {
+    "full": false,
+    "name": "newname",
+    "quota-max-objects": 0,
+    "hashpspool": false,
+    "min-size": 1,
+    "crash-replay-interval": 0,
+    "pg-num": 64,
+    "pgp-num": 64,
+    "quota-max-bytes": 0,
+    "size": 2,
+    "id": 3,
+    "crush-ruleset": 2
+  }
+];
