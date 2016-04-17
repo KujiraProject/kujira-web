4760d3aec9f525f6fa90a4b64d9898ca7403d176
majchrzamemil <majchrzakemil@gmail.com>
2016-04-17 10:26:56 +0200

    updated event-handler-service.js
    removed map

--- kujira/app/services/enevt-handler-service.js
+++ kujira/app/services/enevt-handler-service.js
@@ -1,17 +1,15 @@
-import Ember from 'ember';
+import Ember from 'event-handler-service.js';
 import io from 'socket.io';
 import ENV from 'kujira/config/enviroment';
 
 export default Ember.Service.extend(Ember.Evented, {
     socket: null,
     events: ["osdAdded", "osdRemoved", "OSDup", "OSDdown", "OSDrebalanced"],
-    graphHandlers:new Map(),
 
     onInit: function() {
         this.get('socket').set(io.connect(ENV.APP.API_HOST, ENV.APP.API_PORT, ENV.APP.API_NAMESPACE));
         var socket = this.get('socket');
         socket.on('eventNotification', this.eventNotification, this);
-        alert(ENV.APP.API_HOST);
         socket.on('close', function() {
               this.get('socket').set(io.connect(ENV.APP.API_HOST, ENV.APP.API_PORT, ENV.APP.API_NAMESPACE));
         }, this);
@@ -22,16 +20,14 @@ export default Ember.Service.extend(Ember.Evented, {
         var objMessage=obj.eventType;
         var tmp =Ember.$.inArray(objMessage, this.get('events'));
             if (tmp) {
-                this.trigger(tmp, obj);
+                this.trigger(objMessage, obj);
             }
-        alert(objMessage);
     },
 
-    joinGraph: function(graphType, handler){
+    joinGraph: function(graphType){
         var socket = this.get('socket');
         socket.emit('join',graphType);
         socket.on('graphNotification',this.graphNotification, this);
-        this.get('graphHander').set(graphType,handler);
     },
 
     closeGraph: function(graphType){
@@ -42,6 +38,6 @@ export default Ember.Service.extend(Ember.Evented, {
     graphNotification: function(event){
         var obj =JSON.parse(event);
         var data=[obj.X,obj.Y];
-        this.trigger(this.get('graphHandlers').get(obj.graphType),data);
+        this.trigger(obj.graphType,data);
     }
 });
