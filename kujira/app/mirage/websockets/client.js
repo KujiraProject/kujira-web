e966d4673b3cb08f148a74d2690ae76b72128f8c
tekobylinski <tekobylinski@gmail.com>
2016-04-18 00:28:12 +0200

    Added files via upload

---
+++ kujira/app/mirage/websockets/client.js
@@ -0,0 +1,58 @@
+// import io from 'socket.io';
+// import socket.io-client from "npm:socket.io-client";
+// var io = require('socket.io-client');
+
+// import WebSocket from 'npm:ws';
+
+var wsClient = {
+  socket: null,
+  connect: function() {
+    this.socket = io.connect('http://localhost:8080');
+    this.socket.on('connect', function() {
+
+      console.log('mirage client connected!');
+    });
+
+  },
+  send: function(data, data1) {
+    this.socket.emit('eNotification', data1);
+    this.socket.emit('gNotification', data);
+  }
+
+};
+export default wsClient;
+
+
+
+
+
+// var io = require('socket.io-client');
+// var socket = io('http://localhost:8080');
+//
+// socket.on('connect', function(socket) {
+//
+//   console.log('mirage client connected!');
+// });
+// socket.emit('switchRoom', 'Diagram1');
+// setInterval(function() {
+//   socket.emit('eNotification', {
+//     eventType: 'OSDAdded',
+//     id: 1,
+//     message: 'message'
+//   });
+//   socket.emit('gNotification', {
+//     X: '11',
+//     Y: '22'
+//   });
+// }, 5000);
+//
+//
+// socket.on('eNotificationCall', function(data) {
+//   console.log(data);
+// });
+// socket.on('gNotificationCall', function(data) {
+//   console.log(data);
+// });
+// socket.on('disconnect', function() {
+//
+// });
