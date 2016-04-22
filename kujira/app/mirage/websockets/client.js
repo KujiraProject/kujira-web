015e159a1db8167b17607b697d2f4f34bba18bff
tekobylinski <tekobylinski@gmail.com>
2016-04-22 21:39:33 +0200

    Update client.js

--- kujira/app/mirage/websockets/client.js
+++ kujira/app/mirage/websockets/client.js
@@ -1,8 +1,4 @@
-// import io from 'socket.io';
-// import socket.io-client from "npm:socket.io-client";
-// var io = require('socket.io-client');
 
-// import WebSocket from 'npm:ws';
 
 var wsClient = {
   socket: null,
@@ -14,45 +10,12 @@ var wsClient = {
     });
 
   },
-  send: function(data, data1) {
-    this.socket.emit('eNotification', data1);
-    this.socket.emit('gNotification', data);
+  send: function(eventData, graphData) {
+    this.socket.emit('eNotification', eventData);
+    this.socket.emit('gNotification', graphData);
   }
 
 };
 export default wsClient;
 
 
-
-
-
-// var io = require('socket.io-client');
-// var socket = io('http://localhost:8080');
-//
-// socket.on('connect', function(socket) {
-//
-//   console.log('mirage client connected!');
-// });
-// socket.emit('switchRoom', 'Diagram1');
-// setInterval(function() {
-//   socket.emit('eNotification', {
-//     eventType: 'OSDAdded',
-//     id: 1,
-//     message: 'message'
-//   });
-//   socket.emit('gNotification', {
-//     X: '11',
-//     Y: '22'
-//   });
-// }, 5000);
-//
-//
-// socket.on('eNotificationCall', function(data) {
-//   console.log(data);
-// });
-// socket.on('gNotificationCall', function(data) {
-//   console.log(data);
-// });
-// socket.on('disconnect', function() {
-//
-// });
