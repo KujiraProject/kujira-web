aa1bdf2499760f63ff48985fc28acbce5b951bed
tekobylinski <tekobylinski@gmail.com>
2016-04-22 15:13:41 +0100

    Update environment.js

--- kujira/config/environment.js
+++ kujira/config/environment.js
@@ -19,7 +19,9 @@ module.exports = function(environment) {
       // when it is created
     }
   };
-
+  ENV['ember-cli-mirage'] = {
+    enabled: false
+  };
   if (environment === 'development') {
     // ENV.APP.LOG_RESOLVER = true;
     // ENV.APP.LOG_ACTIVE_GENERATION = true;
