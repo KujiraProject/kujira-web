65df70892e102a492ef69f64c57781b4e019fde8
Ukis <polatowski.lukasz@gmail.com>
2016-04-24 23:23:30 +0100

    checking if role is in array instead of long 'if' statement

--- kujira/app/controllers/settings.js
+++ kujira/app/controllers/settings.js
@@ -3,8 +3,11 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   actions: {
     save(user) {
+      var allRoles = ["superuser", "user", "observer"];
       var role = $( "#user"+user.id+"RoleSelection" ).val();
-      if(role === "superuser" || role === "user" || role === "observer") {
+      var roleFound = $.inArray(role, allRoles);
+      
+      if(roleFound > -1) {
       user.set('role', role);
       user.save();
       }
