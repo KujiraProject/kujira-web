8f293e2c047fc58fa462a4be79cbe6bca0b006b5
tekobylinski <tekobylinski@gmail.com>
2016-05-05 22:58:31 +0100

    Update server.js
    added generating of events, and also setting transport to websocket.

--- kujira/app/mirage/websockets/server.js
+++ kujira/app/mirage/websockets/server.js
@@ -1,11 +1,15 @@
-var app = require('http').createServer(handler);
-var io = require('socket.io')(app);
-app.listen(8080);
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+io.set('transport', ['websocket']);
 
-function handler(req, res) {
-  res.writeHead(200);
-  res.end('default.index');
-}
+app.get('/kujira', function(req, res) {
+  res.sendfile('index.html');
+});
+
+http.listen(8080, function() {
+  console.log('server is running...');
+});
 
 var rooms = {
   'Diagram1': [
@@ -17,8 +21,11 @@ var rooms = {
 };
 
 io.on('connection', function(socket) {
-  console.log('client connected');
+  console.log('user connected');
 
+  socket.on('disconnect', function() {
+    console.log('user disconnected');
+  });
 
   socket.on('join', function(roomName) {
     rooms[roomName].push(socket.id);
@@ -31,23 +38,42 @@ io.on('connection', function(socket) {
     socket.leave('roomName');
   });
 
-
   socket.on('eNotification', function(data) {
     console.log(data);
-    io.to(socket.id).emit('eNotificationCall', data);
-    io.to('LoggedIn').emit('eventNotification', data);
+    console.log('passing eventData to users in room LoggedIn');
+
+    io.to('LoggedIn').emit('eventNotification', generateEventData());
   });
 
 
   socket.on('gNotification', function(data) {
     console.log(data);
-    io.to(socket.id).emit('gNotificationCall', data);
-    io.to('Diagram1').emit('diagram1Notification', data);
+    console.log('passing eventData to users in room Diagram1');
+    io.to('Diagram1').emit('diagram1Notification', generateGraphData());
   });
-
-
-  socket.on('close', function() {
-    console.log('client disconnected');
-  });
-
 });
+
+function generateGraphData() {
+  var graphEvents = {};
+
+  for (var x = 0; x < 100; x++) {
+    graphEvents[x] = {
+      X: x * Math.random(),
+      Y: (x + 2) * Math.random()
+    };
+  }
+  return graphEvents;
+};
+
+function generateEventData() {
+  var notifyEvents = {};
+
+  for (var x = 0; x < 100; x++) {
+    notifyEvents[x] = {
+      eventType: 'OSDAdded',
+      id: x,
+      message: 'message' + x
+    };
+  }
+  return notifyEvents;
+};
