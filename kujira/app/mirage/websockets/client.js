eb4077622dc5faeec2d040cc3a185f34d012dac9
tekobylinski <tekobylinski@gmail.com>
2016-05-05 22:56:36 +0100

    Update client.js

--- kujira/app/mirage/websockets/client.js
+++ kujira/app/mirage/websockets/client.js
@@ -1,19 +1,17 @@
 var wsClient = {
   socket: null,
   connect: function() {
-    this.socket = io.connect('http://localhost:8080');
+    this.socket = io('http://localhost:8080/', {
+      transports: ['websocket']
+    });
     this.socket.on('connect', function() {
-
       console.log('mirage client connected!');
     });
-
   },
-  send: function(eventData, graphData) {
-    this.socket.emit('eNotification', eventData);
-    this.socket.emit('gNotification', graphData);
+  send: function(requestEventData, requestGraphData) {
+    this.socket.emit('eNotification', requestEventData);
+    this.socket.emit('gNotification', requestGraphData);
   }
 
 };
 export default wsClient;
-
-
