6622e10880554dc0c91dca6bc35e00dbbbef7eb1
Tomek Menet <tommen@onet.eu>
2016-05-10 08:34:29 +0100

    BUGS Dispatch
    [1].Session Service

--- kujira/app/services/session.js
+++ kujira/app/services/session.js
@@ -4,24 +4,34 @@ import Ember from 'ember';
 export default ESASession.extend({
 
   store: Ember.inject.service(),
-  suPermision: false,
+  isUser: false,
+  isAdmin: false,
+  isSuser: false,
 
-  setCurrentUser: function() {
-    if (this.get('isAuthenticated')) {
+  setCurrentGroup: function() {
 
-      this.get('store').queryRecord('user', {}).then((user) => {
-        this.set('data.currentUser', user);
-        if(user.get('email') === "suser@suser.com") {
-          this.set('data.suPermision', true);
-        }
-        else {
-          this.set('data.suPermision', false);
-        }
-      });
+          this.set('data.currentUser.group', this.get('data').authenticated.data[1].id);
 
-    }
+          if(this.get('data').currentUser.group === "admins") {
+            this.set('data.isAdmin', true);
+          }
+          else {
+            this.set('data.isAdmin', false);
+          }
+          if(this.get('data').currentUser.group === "susers") {
+            this.set('data.isSuser', true);
+            this.set('data.isAdmin', true);
+          }
+          else {
+            this.set('data.isSuser', false);
+          }
+          if(this.get('data').currentUser.group === "users") {
+            this.set('data.isUser', true);
+          }
+          else {
+            this.set('data.isUser', false);
+          }
+
   }.observes('isAuthenticated'),
-
-
 
 });
