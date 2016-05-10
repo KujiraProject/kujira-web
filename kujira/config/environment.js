505fa8fb9ebfc0a124febaf257cd8abd13376700
Tomek Menet <tommen@onet.eu>
2016-05-10 10:31:55 +0100

    Adding All Files

--- kujira/config/environment.js
+++ kujira/config/environment.js
@@ -4,7 +4,7 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'kujira',
     environment: environment,
-    baseURL: '/kujira',
+    baseURL: '/kujira/',
     locationType: 'auto',
     EmberENV: {
       FEATURES: {
@@ -14,18 +14,68 @@ module.exports = function(environment) {
     },
 
     APP: {
+      NAMESPACE: "kujira/api/v1",
+      SECURED: 'true',
+
       // Here you can pass flags/options to your application instance
       // when it is created
     }
   };
 
-  if (environment === 'development') {
-    // ENV.APP.LOG_RESOLVER = true;
-    // ENV.APP.LOG_ACTIVE_GENERATION = true;
-    // ENV.APP.LOG_TRANSITIONS = true;
-    // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
-    // ENV.APP.LOG_VIEW_LOOKUPS = true;
+  ENV['ember-simple-auth'] = {
+  //authenticationRoute: 'login',
+  baseURL: '/kujira',
+  routeAfterAuthentication: 'dashboard',
+  routeAfterInvalidation: 'login',
+  routeIfAlreadyAuthenticated: 'dashboard',
+  //authorizer: 'simple-auth-authorizer:token'
+  authorizer: 'authorizer:token',
+  crossOriginWhitelist:[
+     'http://localhost:5000'
+  ]
+
   }
+
+
+  ENV['ember-simple-auth-token'] = {
+  serverTokenEndpoint: 'http://localhost:5000/authenticate', /*'api/v1/token',*/ //IF YOU WANT TO USE MIRAGE
+  identificationField: 'username',
+  passwordField: 'password',
+  tokenPropertyName: 'token',
+  /*authorizationPrefix: 'Bearer ',
+  AuthorizationHeaderName: 'Authorization',
+  headers: {},*/
+  refreshAccessTokens: true,
+  serverTokenRefreshEndpoint: '/api/token-refresh/',
+  tokenExpireName: 'exp',
+   timeFactor: 1,
+   refreshLeeway: 300 // Refresh the token 5 minutes (300s) before it expires.//*/
+}
+
+  if (environment === 'development') {
+  // ENV.APP.LOG_RESOLVER = true;
+  // ENV.APP.LOG_ACTIVE_GENERATION = true;
+  // ENV.APP.LOG_TRANSITIONS = true;
+  // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
+  // ENV.APP.LOG_VIEW_LOOKUPS = true;
+  //ENV.APP.API_HOST = "127.0.0.1:5000";
+  ENV['ember-cli-mirage'] = {
+    enabled: false
+    }
+
+  }
+
+
+  if (environment === 'dev-api-local') {
+  // ENV.APP.LOG_RESOLVER = true;
+  // ENV.APP.LOG_ACTIVE_GENERATION = true;
+  // ENV.APP.LOG_TRANSITIONS = true;
+  // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
+  // ENV.APP.LOG_VIEW_LOOKUPS = true;
+  //ENV.APP.API_HOST = "127.0.0.1:5000";
+
+  }
+
 
   if (environment === 'test') {
     // Testem prefers this...
