ce73be6392fbc61b1b4d1d47ecee80edcbcc33a7
majchrzamemil <majchrzakemil@gmail.com>
2016-05-14 17:39:49 +0200

    working service

--- kujira/app/services/event-handler-service.js
+++ kujira/app/services/event-handler-service.js
@@ -1,43 +1,42 @@
 import Ember from 'ember';
-import io from 'socket.io';
-import ENV from 'kujira/config/enviroment';
+//import ENV from 'kujira/config/enviroment';
 
 export default Ember.Service.extend(Ember.Evented, {
-    socket: null,
-    events: ["osdAdded", "osdRemoved", "OSDup", "OSDdown", "OSDrebalanced"],
+            socket: '',
+            events: ["osdAdded", "osdRemoved", "OSDup", "OSDdown", "OSDrebalanced"],
 
-    onInit: function() {
-        this.get('socket').set(io.connect(ENV.APP.API_HOST, ENV.APP.API_PORT, ENV.APP.API_NAMESPACE));
-        var socket = this.get('socket');
-        socket.on('eventNotification', this.eventNotification, this);
-        socket.on('close', function() {
-              this.get('socket').set(io.connect(ENV.APP.API_HOST, ENV.APP.API_PORT, ENV.APP.API_NAMESPACE));
-        }, this);
-    }.on('init'),
+            init: function() {
+                let self = this;
+                this.set('socket', io.connect('http://localhost:7000', {
+                    transports: ['websocket']
+                }));
+                var socket = this.get('socket');
+                let eventNotification = function(message) {
+                    var tmp = Ember.$.inArray(message.eventType, self.events);
+                    if (tmp) {
+                        self.trigger(message.eventType, message.message);
+                    }
+                };
 
-    eventNotification: function(message) {
-        var obj = JSON.parse(message);
-        var objMessage=obj.eventType;
-        var tmp =Ember.$.inArray(objMessage, this.get('events'));
-            if (tmp) {
-                this.trigger(objMessage, obj);
-            }
-    },
+                let graphNotification = function(event) {
+                    self.trigger(event.Room, event.Data);
+                };
+                socket.on('event notification', eventNotification, this);
+                socket.on('graph notification', graphNotification, this);
+                socket.on('close', function() {
+                        this.set('socket', io.connect('http://localhost:7000', {
+                            transports: ['websocket']
+                        }, this));
 
-    joinGraph: function(graphType){
-        var socket = this.get('socket');
-        socket.emit('join',graphType);
-        socket.on('graphNotification',this.graphNotification, this);
-    },
+                    },
 
-    closeGraph: function(graphType){
-        var socket = this.get('socket');
-        socket.emit('leave',graphType);
-    },
+                    joinGraph: function(graphType) {
+                        this.socket.emit('join', graphType);
+                    },
 
-    graphNotification: function(event){
-        var obj =JSON.parse(event);
-        var data=[obj.X,obj.Y];
-        this.trigger(obj.graphType,data);
-    }
-});
+                    closeGraph: function(graphType) {
+                        this.socket.emit('leave', graphType);
+                    },
+
+
+                });
