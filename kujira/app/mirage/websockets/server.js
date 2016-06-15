56df999935a46fc705b8284568d5d5c6502a7ffa
tekobylinski <tekobylinski@gmail.com>
2016-06-15 20:34:32 +0200

    Update server.js

--- kujira/app/mirage/websockets/server.js
+++ kujira/app/mirage/websockets/server.js
@@ -4,74 +4,77 @@ var io = require('socket.io')(http);
 io.set('transport', ['websocket']);
 
 app.get('/kujira', function(req, res) {
-  res.sendfile('index.html');
+    res.sendfile('index.html');
 });
 
 http.listen(7000, function() {
-  console.log('server is running...');
+    console.log('server is running...');
 });
 var eventFromMirage;
 var rooms = {
-  'Events': [
+    'Events': [
 
-  ],
-  'Graph1': [
+    ],
+    'Graph1': [
 
-  ],
-  'Graph2': [
+    ],
+    'Graph2': [
 
-  ],
-  'Graph3': [
+    ],
+    'Graph3': [
 
-  ]
+    ]
 };
 
 io.on('connection', function(socket) {
-  console.log('user connected');
+    console.log('user connected');
 
-  socket.on('disconnect', function() {
-    console.log('user disconnected');
-  });
+    socket.on('disconnect', function() {
+        for (var room in rooms) {
+            var toRemove = rooms[room].indexOf(socket.id);
+            if (toRemove === null) {} else {
+                rooms[room].splice(toRemove, 1);
+            }
+            socket.leave(room);
+        }
+        console.log('user disconnected');
+    });
 
-  socket.on('join', function(roomName) {
-    console.log('User with id   ' + socket.id + '     joined the room   ' + roomName.room);
-    socket.join(roomName.room);
-    if(roomName.room == 'Events') {
-      io.to(roomName.room).emit(eventFromMirage.eventType, eventFromMirage.data);
-    }
-    rooms[roomName.room].push(socket.id);
+    socket.on('join', function(roomName) {
+        console.log('User with id   ' + socket.id + '     joined the room   ' + roomName.room);
+        socket.join(roomName.room);
+        if (roomName.room == 'Events') {
+            io.to(roomName.room).emit(eventFromMirage.eventType, eventFromMirage.data);
+        }
+        rooms[roomName.room].push(socket.id);
+    });
 
-  });
+    socket.on('mirageEvent', function(event) {
+        eventFromMirage = event;
+        io.to('Events').emit(event.eventType, event.data);
+    });
 
-  socket.on('mirageEvent', function(event) {
-    eventFromMirage = event;
-    io.to('Events').emit(event.eventType, event.data);
-  });
+    socket.on('leave', function(roomName) {
+        console.log('User with id   ' + socket.id + '     left room   ' + roomName);
+        var toRemove = rooms[roomName].indexOf(socket.id);
+        rooms[roomName].splice(toRemove, 1);
+        socket.leave(roomName);
+    });
 
-  socket.on('leave', function(roomName) {
-    console.log('User with id   ' + socket.id + '     left room   ' + roomName);
-    var toRemove = rooms[roomName].indexOf(socket.id);
-    rooms[roomName].splice(toRemove, 1);
-    socket.leave(roomName);
-  });
-
-  setInterval(function() {
-    io.to('Graph1').emit('graph notification', generateGraphData('Graph1'));
-    io.to('Graph2').emit('graph notification', generateGraphData('Graph2'));
-    io.to('Graph3').emit('graph notofication', generateGraphData('Graph3'));
-  }, Math.floor((Math.random() * 10000) + 5000));
-
-
-
+    setInterval(function() {
+        io.to('Graph1').emit('graph notification', generateGraphData('Graph1'));
+        io.to('Graph2').emit('graph notification', generateGraphData('Graph2'));
+        io.to('Graph3').emit('graph notofication', generateGraphData('Graph3'));
+    }, Math.floor((Math.random() * 10000) + 5000));
 });
 
 function generateGraphData(roomName) {
-  var graphEvent = {
-    Room: roomName,
-    Data: {
-      X: 2 * Math.random(),
-      Y: (5 + 2) * Math.random()
-    }
-  };
-  return graphEvent;
+    var graphEvent = {
+        Room: roomName,
+        Data: {
+            X: 2 * Math.random(),
+            Y: (5 + 2) * Math.random()
+        }
+    };
+    return graphEvent;
 };
